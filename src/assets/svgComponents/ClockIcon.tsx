1e0d341e7625c5cb2d1c6e5df6761886d133dc26
유림 <yulimh001@gmail.com>
2025-06-04 15:01:27 +0900

    fix: pretter 서식 맞춤

--- src/assets/svgComponents/ClockIcon.tsx
+++ src/assets/svgComponents/ClockIcon.tsx
@@ -1,6 +1,26 @@
-import * as React from "react";
-import type { SVGProps } from "react";
-const SvgClockIcon = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 20 21" {...props}><mask id="clock-icon_svg__a" width={20} height={21} x={0} y={0} maskUnits="userSpaceOnUse" style={{
-    maskType: "alpha"
-  }}><path fill="#D9D9D9" d="M0 .88h20v20H0z" /></mask><g mask="url(#clock-icon_svg__a)"><path fill="#6F717C" d="M10.703 10.294V6.896q0-.3-.2-.501a.67.67 0 0 0-.499-.203.69.69 0 0 0-.502.203.68.68 0 0 0-.205.5v3.692q0 .156.058.283.06.127.157.225l2.614 2.614a.66.66 0 0 0 .501.208.72.72 0 0 0 .498-.225.684.684 0 0 0-.007-1zM10 18.38a7.3 7.3 0 0 1-2.914-.586 7.6 7.6 0 0 1-2.389-1.611 7.6 7.6 0 0 1-1.611-2.392 7.3 7.3 0 0 1-.586-2.92q0-1.554.586-2.92a7.4 7.4 0 0 1 1.611-2.384A7.7 7.7 0 0 1 7.09 3.966a7.3 7.3 0 0 1 2.92-.586q1.554 0 2.92.59a7.6 7.6 0 0 1 2.38 1.604 7.6 7.6 0 0 1 1.601 2.38 7.3 7.3 0 0 1 .59 2.926 7.3 7.3 0 0 1-.586 2.914 7.7 7.7 0 0 1-1.602 2.389A7.44 7.44 0 0 1 10 18.38" /></g></svg>;
-export default SvgClockIcon;
+import * as React from 'react'
+import type { SVGProps } from 'react'
+const SvgClockIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 20 21" {...props}>
+    <mask
+      id="clock-icon_svg__a"
+      width={20}
+      height={21}
+      x={0}
+      y={0}
+      maskUnits="userSpaceOnUse"
+      style={{
+        maskType: 'alpha',
+      }}
+    >
+      <path fill="#D9D9D9" d="M0 .88h20v20H0z" />
+    </mask>
+    <g mask="url(#clock-icon_svg__a)">
+      <path
+        fill="#6F717C"
+        d="M10.703 10.294V6.896q0-.3-.2-.501a.67.67 0 0 0-.499-.203.69.69 0 0 0-.502.203.68.68 0 0 0-.205.5v3.692q0 .156.058.283.06.127.157.225l2.614 2.614a.66.66 0 0 0 .501.208.72.72 0 0 0 .498-.225.684.684 0 0 0-.007-1zM10 18.38a7.3 7.3 0 0 1-2.914-.586 7.6 7.6 0 0 1-2.389-1.611 7.6 7.6 0 0 1-1.611-2.392 7.3 7.3 0 0 1-.586-2.92q0-1.554.586-2.92a7.4 7.4 0 0 1 1.611-2.384A7.7 7.7 0 0 1 7.09 3.966a7.3 7.3 0 0 1 2.92-.586q1.554 0 2.92.59a7.6 7.6 0 0 1 2.38 1.604 7.6 7.6 0 0 1 1.601 2.38 7.3 7.3 0 0 1 .59 2.926 7.3 7.3 0 0 1-.586 2.914 7.7 7.7 0 0 1-1.602 2.389A7.44 7.44 0 0 1 10 18.38"
+      />
+    </g>
+  </svg>
+)
+export default SvgClockIcon
--- src/assets/svgComponents/SearchIcon.tsx
+++ src/assets/svgComponents/SearchIcon.tsx
@@ -1,6 +1,26 @@
-import * as React from "react";
-import type { SVGProps } from "react";
-const SvgSearchIcon = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 25 25" {...props}><mask id="search-icon_svg__a" width={25} height={25} x={0} y={0} maskUnits="userSpaceOnUse" style={{
-    maskType: "alpha"
-  }}><path fill="#D9D9D9" d="M.5.342h24v24H.5z" /></mask><g mask="url(#search-icon_svg__a)"><path fill="#5551F5" d="m20.1 21.342-6.3-6.3q-.75.6-1.725.95t-2.075.35q-2.725 0-4.612-1.888T3.5 9.842t1.888-4.613T10 3.342t4.613 1.887T16.5 9.842a6.1 6.1 0 0 1-1.3 3.8l6.3 6.3zm-10.1-7q1.875 0 3.188-1.313Q14.5 11.717 14.5 9.842t-1.312-3.188T10 5.342 6.813 6.654 5.5 9.842t1.313 3.187T10 14.342" /></g></svg>;
-export default SvgSearchIcon;
+import * as React from 'react'
+import type { SVGProps } from 'react'
+const SvgSearchIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 25 25" {...props}>
+    <mask
+      id="search-icon_svg__a"
+      width={25}
+      height={25}
+      x={0}
+      y={0}
+      maskUnits="userSpaceOnUse"
+      style={{
+        maskType: 'alpha',
+      }}
+    >
+      <path fill="#D9D9D9" d="M.5.342h24v24H.5z" />
+    </mask>
+    <g mask="url(#search-icon_svg__a)">
+      <path
+        fill="#5551F5"
+        d="m20.1 21.342-6.3-6.3q-.75.6-1.725.95t-2.075.35q-2.725 0-4.612-1.888T3.5 9.842t1.888-4.613T10 3.342t4.613 1.887T16.5 9.842a6.1 6.1 0 0 1-1.3 3.8l6.3 6.3zm-10.1-7q1.875 0 3.188-1.313Q14.5 11.717 14.5 9.842t-1.312-3.188T10 5.342 6.813 6.654 5.5 9.842t1.313 3.187T10 14.342"
+      />
+    </g>
+  </svg>
+)
+export default SvgSearchIcon
