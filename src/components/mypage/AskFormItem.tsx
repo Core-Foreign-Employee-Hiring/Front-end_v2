7c2e6cf1ba02114f81c0cefab32776c458c25e28
유림 <yulimh001@gmail.com>
2025-09-09 23:45:45 +0900

    style: 문의 페이지 디자인 변경 반영

--- src/components/mypage/AskFormItem.tsx
+++ src/components/mypage/AskFormItem.tsx
@@ -1,4 +1,4 @@
-import { QnaArrowDownIcon, QnaArrowUpIcon } from '@/assets/svgComponents'
+import { BentArrowIcon, QnaArrowDownIcon, QnaArrowUpIcon } from '@/assets/svgComponents'
 import { InquiryType } from '@/types/archive'
 import { useState } from 'react'
 import { useModalStore } from '@/store/modalStore'
@@ -17,25 +17,27 @@ export default function AskFormItem({ type, isAnswered, answer, archiveInquiryId
         onClick={() => {
           setIsQnADropDownOpen(!isQnADropDownOpen)
         }}
-        className={'border-gray2 flex flex-col gap-y-2 border-b px-3 py-4'}
+        className={'border-gray2 flex flex-col gap-y-3 border-b px-3 py-4'}
       >
-        <p className="subtitle-lg">{title}</p>
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-x-2">
-            <div className="bg-main subtitle-md flex h-[36px] w-[36px] items-center justify-center rounded-full text-white">
-              Q
+        <div className="badge-md text-main bg-main-light w-fit rounded-[8px] px-2 py-1">답변완료</div>
+        <div className="flex flex-col gap-y-1">
+          <p className="subtitle-lg">{title}</p>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-x-2">
+              <p className="body-md text-gray5">{inquiry}</p>
             </div>
-            <p className="body-md">{inquiry}</p>
+            {isQnADropDownOpen ? (
+              <QnaArrowUpIcon width={18} height={24} />
+            ) : (
+              <QnaArrowDownIcon width={18} height={24} />
+            )}
           </div>
-          {isQnADropDownOpen ? <QnaArrowUpIcon width={18} height={24} /> : <QnaArrowDownIcon width={18} height={24} />}
         </div>
       </section>
       {isQnADropDownOpen && (
         <section className="bg-gray1 border-gray2 border-b px-3 py-4">
           <div className="flex items-center gap-x-2">
-            <div className="border-main subtitle-md text-main flex h-[36px] w-[36px] items-center justify-center rounded-full border bg-white">
-              A
-            </div>
+            <BentArrowIcon width={12} height={10} />
             <p className="body-md text-gray5">{answer}</p>
           </div>
         </section>
@@ -43,25 +45,25 @@ export default function AskFormItem({ type, isAnswered, answer, archiveInquiryId
     </div>
   ) : (
     <div className="border-gray2 flex flex-col border-b">
-      <section className={'flex flex-col gap-y-2 px-3 py-4'}>
-        <p className="subtitle-lg">{title}</p>
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-x-2">
-            <div className="bg-main subtitle-md flex h-[36px] w-[36px] items-center justify-center rounded-full text-white">
-              Q
+      <section className={'flex flex-col gap-y-3 px-3 py-4'}>
+        <div className="badge-md text-gray5 bg-gray2 w-fit rounded-[8px] px-2 py-1">답변대기</div>
+        <div className="flex flex-col gap-y-1">
+          <p className="subtitle-lg">{title}</p>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-x-2">
+              <p className="body-md text-gray5">{inquiry}</p>
             </div>
-            <p className="body-md text-gray5">{inquiry}</p>
+            {type === '내가 받은 문의' && (
+              <button
+                onClick={() => {
+                  setState({ isAnswerModalOpen: true, selectedInquiryId: archiveInquiryId })
+                }}
+                className="small border-gray2 text-gray5 rounded-[8px] border px-3 py-2"
+              >
+                답변남기기
+              </button>
+            )}
           </div>
-          {type === '내가 받은 문의' && (
-            <button
-              onClick={() => {
-                setState({ isAnswerModalOpen: true, selectedInquiryId: archiveInquiryId })
-              }}
-              className="small border-gray2 text-gray5 rounded-[8px] border px-3 py-2"
-            >
-              답변남기기
-            </button>
-          )}
         </div>
       </section>
     </div>
