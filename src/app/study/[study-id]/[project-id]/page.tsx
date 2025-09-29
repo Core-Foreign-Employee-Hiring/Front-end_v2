02e3a7a81fbdd146c0a203b3c6a056b68ed5f135
유림 <yulimh001@gmail.com>
2025-09-30 00:40:42 +0900

    feat: 스터디 페이지 데이터 추가

--- src/app/study/[study-id]/[project-id]/page.tsx
+++ src/app/study/[study-id]/[project-id]/page.tsx
@@ -1,36 +1,42 @@
 'use client'
 
 import Header from '@/components/common/Header'
-import Button from '@/components/common/Button'
 import Image from 'next/image'
-import { GrayRightArrowIcon } from '@/assets/svgComponents'
-import { ROOMIOData } from '@/utils/study'
+import { EZIPData, KODICData, KORIData, KuuidData, ROOMIOData, StudyDataType } from '@/utils/study'
+import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 export default function ProjectDetailPage() {
+  const pathName = usePathname()
+  const [data, setData] = useState<StudyDataType>()
+
+  useEffect(() => {
+    switch (pathName.split('/')[3]) {
+      case '1':
+        return setData(ROOMIOData)
+      case '2':
+        return setData(EZIPData)
+      case '3':
+        return setData(KuuidData)
+      case '4':
+        return setData(KORIData)
+      default:
+        return setData(KODICData)
+    }
+  }, [pathName])
+
   return (
     <main className="relative mx-auto min-h-screen w-[375px] bg-white">
       <div className="">
-        <Header headerType={'dynamic'} title={'KORI'} />
+        <Header headerType={'dynamic'} title={data?.projectTitle} />
         <div className="h-[40px]" />
         <div className="mt-[32px] flex flex-col gap-y-[20px] px-5">
           <section className="flex justify-between">
-            <div className="relative h-[32px] w-[93px]">
-              <Image src={'/logo.svg'} alt={'로고'} className="object-cover" fill />
+            <div className="relative h-[50px] w-[50px]">
+              <Image src={data?.logoImage ?? '/logo.svg'} alt={'로고'} className="object-cover" fill />
             </div>
-            <Button
-              onClick={() => {}}
-              type={'outline'}
-              size={'sm'}
-              rightIcon={<GrayRightArrowIcon width={12} height={12} />}
-            >
-              바로가기
-            </Button>
           </section>
-          <p className="body-md">
-            KORI는 KOR’ea + 고‘리’의 의미를 담아 외국인 노동자의 안정적인 한국 생활과 정착을 돕는 플랫폼입니다. 앱을
-            통해 맞춤형 안전·직무 교육을 제공하고, 학습 기록은 자동으로 포트폴리오(이수증)로 정리됩니다. 정부와
-            사업주에게는 참여율 데이터를 제공하며, 학습자에게는 포인트와 인센티브를 통해 지속적인 학습을 유도합니다.
-          </p>
+          <p className="body-md">{data?.description}</p>
           <section className="border-gray2 flex flex-col gap-y-3 rounded-[20px] border p-5">
             <div className="flex items-center">
               <div className="badge-sm text-gray4 w-[80px]">프로젝트 설명</div>
@@ -44,33 +50,22 @@ export default function ProjectDetailPage() {
               <div className="badge-sm text-gray4 w-[80px]">프로젝트 기간</div>
               <p className="body-sm text-gray5">2025. 09. 13 - 2025. 09. 14</p>
             </div>
-            <div className="flex">
-              <div className="badge-sm text-gray4 w-[80px]">팀 구성</div>
-              <div className="flex flex-col gap-y-1">
-                <div className="body-sm text-gray5 flex items-center gap-x-3">
-                  <div className="w-[60px]">기획</div>
-                  <p>|</p>
-                  <p>이수민, 정재원</p>
-                </div>
-                <div className="body-sm text-gray5 flex items-center gap-x-3">
-                  <div className="w-[60px]">디자인</div>
-                  <p>|</p>
-                  <p>김예림</p>
-                </div>
-                <div className="body-sm text-gray5 flex items-center gap-x-3">
-                  <div className="w-[60px]">개발</div>
-                  <p>|</p>
-                  <p>최태진, Rashed</p>
-                </div>
-              </div>
+            <div className="flex items-center">
+              <div className="badge-sm text-gray4 w-[80px]">팀원</div>
+              <div className="body-sm text-gray5">{data?.team}</div>
             </div>
             <div className="relative h-[188px] w-full rounded-[16px]">
-              <Image src={'/pizza.png'} alt="사진" className="rounded-[16px] object-cover" fill></Image>
+              <Image
+                src={data?.teamImage ?? '/logo.svg'}
+                alt="사진"
+                className="rounded-[16px] object-cover"
+                fill
+              ></Image>
             </div>
           </section>
         </div>
         <div className="mt-[24px]">
-          {ROOMIOData.pptImageUrls.map((pptImageUrl) => {
+          {data?.pptImageUrls.map((pptImageUrl) => {
             return <Image alt="최종 장표 사진" src={pptImageUrl} key={pptImageUrl} width={375} height={210}></Image>
           })}
         </div>
--- src/utils/study.ts
+++ src/utils/study.ts
@@ -1,4 +1,22 @@
-export const ROOMIOData = {
+export interface StudyDataType {
+  teamName: string
+  projectTitle: string
+  teamImage: string
+  logoImage: string
+  oneLineDescription: string
+  description: string
+  team: string
+  pptImageUrls: string[]
+}
+export const ROOMIOData: StudyDataType = {
+  teamName: '스페이스',
+  projectTitle: 'ROOMIO',
+  teamImage: '/study/git/project/ROOMIO/a_team_img.jpeg',
+  logoImage: '/study/git/project/ROOMIO/A_logo.png',
+  oneLineDescription: '한국 생활이 외로운 외국인 친구들을 위한 공간 공유 플랫폼, Roomio',
+  description:
+    'Roomio는 외국인들이 한국에서 겪는 언어 문제와 외로움을 해결하는 데 초점을 맞추고 있습니다. 단순히 공간만 빌려주는 게 아니라,지역 커뮤니티를 만들고 문화 콘텐츠를 함께 즐길 수 있게 합니다. 우리 동네 가까운 이웃들과 자동으로 매칭되어 같이 모임을 만들고, 다양한 활동을 하며 친구를 사귈 수 있습니다!',
+  team: '한혜원, 한성진, 신지수, 김준홍, 최지웅',
   pptImageUrls: [
     '/study/git/project/ROOMIO/1.png',
     '/study/git/project/ROOMIO/2.png',
@@ -12,5 +30,120 @@ export const ROOMIOData = {
     '/study/git/project/ROOMIO/10.png',
     '/study/git/project/ROOMIO/11.png',
     '/study/git/project/ROOMIO/12.png',
+  ],
+}
+export const EZIPData: StudyDataType = {
+  teamName: 'Konnection',
+  projectTitle: 'EZIP',
+  logoImage: '/study/git/project/EZIP/B_logo.jpeg',
+  teamImage: '/study/git/project/EZIP/b_team_img.jpeg',
+  oneLineDescription: '한국에서 집 구하기 막막한 외국인 친구들을 위한 똑똑한 플랫폼, EZIP',
+  description:
+    'EZIP은 한국 거주 외국인들이 겪는 언어 장벽이나 사기 피해 같은 문제들을 해결해 주기 위해 만들었습니다.\n' +
+    '복잡한 계약서도 어려운 법률 용어 없이 쉽게 설명해주고, 집주인과 실시간 번역 채팅으로 편하게 대화할 수 있습니다. 게다가 다른 외국인들의 생생한 후기까지 볼 수 있어서, 안심하고 믿을 수 있는 집을 찾을 수 있습니다.',
+  team: '알리, 조효리, 윤수진, 김유빈, 곽태근',
+  pptImageUrls: [
+    '/study/git/project/EZIP/1.png',
+    '/study/git/project/EZIP/2.png',
+    '/study/git/project/EZIP/3.png',
+    '/study/git/project/EZIP/4.png',
+    '/study/git/project/EZIP/5.png',
+    '/study/git/project/EZIP/6.png',
+    '/study/git/project/EZIP/7.png',
+    '/study/git/project/EZIP/8.png',
+    '/study/git/project/EZIP/9.png',
+    '/study/git/project/EZIP/10.png',
+    '/study/git/project/EZIP/11.png',
+    '/study/git/project/EZIP/12.png',
+  ],
+}
+export const KuuidData: StudyDataType = {
+  teamName: 'C앗',
+  projectTitle: 'Kuuid',
+  logoImage: '/study/git/project/Kuide/C_logo.png',
+  teamImage: '/study/git/project/Kuide/c_team_img.jpeg',
+  oneLineDescription: 'AI가 도와주는 한국 생활 시뮬레이션! Kuuid',
+  description:
+    'Kuuid는 국내에 체류하는 외국인들이 겪는 가장 큰 두 가지 어려움, 바로 복잡한 행정 처리와 실생활 한국어 학습을 동시에 해결해주는 AI 기반 플랫폼입니다.\n' +
+    '비자 연장, 은행 계좌 개설, 휴대폰 개통 등 실제 한국에서 처리해야 하는 복잡한 행정 업무를 Kuuid의 AI 공무원과 실제 과정을 시뮬레이션하며 미리 경험할 수 있습니다.',
+  team: '사라, 박수빈, 유창권, 김수현, 조현호',
+  pptImageUrls: [
+    '/study/git/project/Kuide/1.png',
+    '/study/git/project/Kuide/2.png',
+    '/study/git/project/Kuide/3.png',
+    '/study/git/project/Kuide/4.png',
+    '/study/git/project/Kuide/5.png',
+    '/study/git/project/Kuide/6.png',
+    '/study/git/project/Kuide/7.png',
+    '/study/git/project/Kuide/8.png',
+    '/study/git/project/Kuide/9.png',
+    '/study/git/project/Kuide/10.png',
+    '/study/git/project/Kuide/11.png',
+    '/study/git/project/Kuide/12.png',
+    '/study/git/project/Kuide/13.png',
+    '/study/git/project/Kuide/14.png',
+    '/study/git/project/Kuide/15.png',
+    '/study/git/project/Kuide/16.png',
+    '/study/git/project/Kuide/17.png',
+    '/study/git/project/Kuide/18.png',
+    '/study/git/project/Kuide/19.png',
+    '/study/git/project/Kuide/20.png',
+    '/study/git/project/Kuide/21.png',
+    '/study/git/project/Kuide/22.png',
+    '/study/git/project/Kuide/23.png',
+    '/study/git/project/Kuide/24.png',
+    '/study/git/project/Kuide/25.png',
+    '/study/git/project/Kuide/26.png',
+    '/study/git/project/Kuide/27.png',
+  ],
+}
+export const KORIData: StudyDataType = {
+  teamName: 'TOGETHER KOREA',
+  projectTitle: 'KORI',
+  logoImage: '/study/git/project/KORI/D_logo.jpeg',
+  teamImage: '/study/git/project/KORI/d_team_img.jpeg',
+  oneLineDescription: '외국인 노동자의 안정적인 한국 생활과 정착을 돕는 플랫폼',
+  description:
+    'KORI는 KOR’ea + 고‘리’의 의미를 담아 외국인 노동자의 안정적인 한국 생활과 정착을 돕는 플랫폼입니다. 앱을\n' +
+    '            통해 맞춤형 안전·직무 교육을 제공하고, 학습 기록은 자동으로 포트폴리오(이수증)로 정리됩니다. 정부와\n' +
+    '            사업주에게는 참여율 데이터를 제공하며, 학습자에게는 포인트와 인센티브를 통해 지속적인 학습을 유도합니다.',
+  team: 'Rashed, 김예림, 최태원, 정재원, 이수민',
+  pptImageUrls: [
+    '/study/git/project/KORI/1.png',
+    '/study/git/project/KORI/2.png',
+    '/study/git/project/KORI/3.png',
+    '/study/git/project/KORI/4.png',
+    '/study/git/project/KORI/5.png',
+    '/study/git/project/KORI/6.png',
+    '/study/git/project/KORI/7.png',
+    '/study/git/project/KORI/8.png',
+    '/study/git/project/KORI/9.png',
+    '/study/git/project/KORI/10.png',
+    '/study/git/project/KORI/11.png',
+    '/study/git/project/KORI/12.png',
+    '/study/git/project/KORI/13.png',
+  ],
+}
+export const KODICData: StudyDataType = {
+  teamName: 'базар',
+  projectTitle: 'KO-DIC',
+  logoImage: '/study/git/project/KO-DIC/E_logo.png',
+  teamImage: '/study/git/project/KO-DIC/e_team_img.jpeg',
+  oneLineDescription: '외국인 유학생의 한국 생활 적응을 돕는 문화 사전 앱, Базар!',
+  description:
+    'GPS 기반 알림 시스템을 통해 특정 장소에 도착하면 “교회에서는 조용히 해야 해요”, “에스컬레이터에서는 오른쪽에 서고 왼쪽은 걸어가는 공간이에요”와 같이 한국의 문화와 예절을 쉽고 직관적으로 안내합니다\n' +
+    '또한 맛집, 카페, 전통 명소 등 현지인이 추천하는 스팟을 함께 제공해, 외국인 유학생이 한국 문화를 더 깊이 이해하고 즐길 수 있도록 돕습니다.',
+  team: '문크오윤, 이민재, 표상혁, 주혜오, 최유정, 황유림',
+  pptImageUrls: [
+    '/study/git/project/KO-DIC/1.png',
+    '/study/git/project/KO-DIC/2.png',
+    '/study/git/project/KO-DIC/3.png',
+    '/study/git/project/KO-DIC/4.png',
+    '/study/git/project/KO-DIC/5.png',
+    '/study/git/project/KO-DIC/6.png',
+    '/study/git/project/KO-DIC/7.png',
+    '/study/git/project/KO-DIC/8.png',
+    '/study/git/project/KO-DIC/9.png',
+    '/study/git/project/KO-DIC/10.png',
   ],
 }
