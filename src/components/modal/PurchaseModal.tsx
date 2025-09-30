6ba284e54fb11c3777cd902b66156f2d30e2d921
유림 <yulimh001@gmail.com>
2025-09-30 14:08:28 +0900

    style: 구매 모달 디자인 변경

--- src/components/modal/PurchaseModal.tsx
+++ src/components/modal/PurchaseModal.tsx
@@ -1,5 +1,7 @@
 import MiddleModal from '@/components/common/MiddleModal'
 import { Dispatch, SetStateAction } from 'react'
+import { PurchaseGraphicsIcon } from '@/assets/svgComponents'
+import Button from '@/components/common/Button'
 
 interface PurchaseModalProps {
   isModalOpen: boolean
@@ -9,13 +11,28 @@ interface PurchaseModalProps {
 export default function PurchaseModal({ isModalOpen, setIsModalOpen }: PurchaseModalProps) {
   return (
     <MiddleModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} modalType={'GENERAL'}>
-      <div>
-        <p className="title-md">
-          <span className="text-main">구매</span>가 완료되었어요!
-        </p>
-        <p className="body-sm">
-          구매한 내역은 <span className="subtitle-sm">마이페이지 → 내 아카이브</span>에서 확인할 수 있어요.
-        </p>
+      <div className="flex flex-col gap-y-[40px]">
+        <div className="flex flex-col gap-y-2">
+          <p className="title-md">
+            <span className="text-main">구매</span>가 완료되었어요!
+          </p>
+          <p className="body-md text-gray5">
+            구매한 내역은
+            <br />
+            <span className="subtitle-md">마이페이지 → 내 아카이브</span>에서 <br />
+            확인할 수 있어요.
+          </p>
+        </div>
+        <PurchaseGraphicsIcon width={300} height={200} />
+        <Button
+          buttonType={'button'}
+          onClick={() => {}}
+          type={'outline'}
+          size={'lg'}
+          customClassName={'w-full bg-white'}
+        >
+          닫기
+        </Button>
       </div>
     </MiddleModal>
   )
