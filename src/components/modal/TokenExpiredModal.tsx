107e27305099da09b4e11cb20de2fef555e65b7b
유림 <yulimh001@gmail.com>
2025-09-30 14:44:38 +0900

    fix: 로그인 버튼을 눌렀을 때, 모달창 닫히도록 수정

--- src/components/modal/TokenExpiredModal.tsx
+++ src/components/modal/TokenExpiredModal.tsx
@@ -1,16 +1,25 @@
 'use client'
 import MiddleModal from '@/components/common/MiddleModal'
 import Link from 'next/link'
+import { useModalStore } from '@/store/modalStore'
 
 interface TokenExpiredModalProps {
   isModalOpen: boolean
 }
 
 export default function TokenExpiredModal({ isModalOpen }: TokenExpiredModalProps) {
+  const setState = useModalStore((state) => state.setState)
   return (
     <MiddleModal isModalOpen={isModalOpen} modalType={'GENERAL'}>
       <p>로그인을 다시 진행해주세요.</p>
-      <Link href={'/login'}>로그인</Link>
+      <Link
+        href={'/login'}
+        onClick={() => {
+          setState({ isTokenExpiredModalOpen: false })
+        }}
+      >
+        로그인
+      </Link>
     </MiddleModal>
   )
 }
